e505c13090becdcac222f7bc1efa396609eeb34d
SenYu <senyu@mail.dlut.edu.cn>
2014-07-24 11:17:19 +0800

    test^#^^

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    jshint: {
+/*    jshint: {
       all: [
         'Gruntfile.js',
         'tasks/*.js',
@@ -21,7 +21,7 @@ module.exports = function(grunt) {
       options: {
         jshintrc: '.jshintrc'
       }
-    },
+    },*/
 
     // Before generating any new files, remove any previously-created files.
     clean: {
@@ -43,31 +43,38 @@ module.exports = function(grunt) {
           punctuation: ' !!!'
         },
         files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+          'tmp/custom_options': ['http://my.m.taobao.com/h5/staticBytes/increLoad.js', 'test/fixtures/123','http://my.m.taobao.com/h5/staticBytes/increLoad.js']
+        }
+      },
+      diff: {
+        options: {
+        },
+        files: {
+          'tmp/diff': ['http://my.m.taobao.com/h5/staticBytes/increLoad.js']
         }
       }
     },
 
-    // Unit tests.
+/*    // Unit tests.
     nodeunit: {
       tests: ['test/*_test.js']
     }
-
+*/
   });
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  //grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  //grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'incrediff', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'incrediff'/*, 'nodeunit'*/]);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', [/*'jshint', */'test']);
 
 };
--- tasks/incrediff.js
+++ tasks/incrediff.js
@@ -17,23 +17,31 @@ module.exports = function(grunt) {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       punctuation: '.',
-      separator: ', '
+      separator: ', ',
+      version: ["1.3.51","1.3.5","1.3.41","1.3.3","1.3.2"],
+      CDN: "http://g.tbcdn.cn"
     });
 
     // Iterate over all specified file groups.
     this.files.forEach(function(f) {
       // Concat specified files.
+      grunt.log.warn(JSON.stringify(f));
+      var dd = [];
       var src = f.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
+       //grunt.log.writeln();
+        dd.push( filepath + grunt.file.exists(filepath) );
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
+          return true;
         } else {
+          grunt.log.warn('Source file "' + filepath + '" found.');
           return true;
         }
       }).map(function(filepath) {
         // Read file source.
-        return grunt.file.read(filepath);
+        grunt.log.writeln(filepath);
+        return grunt.file.read('http://my.m.taobao.com/h5/staticBytes/increLoad.js');
       }).join(grunt.util.normalizelf(options.separator));
 
       // Handle options.
@@ -44,6 +52,7 @@ module.exports = function(grunt) {
 
       // Print a success message.
       grunt.log.writeln('File "' + f.dest + '" created.');
+      grunt.log.writeln(dd.join('.')+dd.length);
     });
   });
 
