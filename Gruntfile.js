443999e8f12c18c0e10a668eb2fe9ba9e9802a56
一杉 <yusen.ys@alibaba-inc.com>
2014-07-29 09:57:40 +0800

    new version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,164 +1,72 @@
+/*
+ * grunt-incrediff
+ * https://github.com/SenYu/grunt-incrediff
+ *
+ * Copyright (c) 2014 SenYu
+ * Licensed under the MIT license.
+ */
+
 'use strict';
 
 module.exports = function(grunt) {
-    var pkg = grunt.file.readJSON('package.json');
-/****************************************/
     var version = grunt.file.readJSON('version.json');
 /****************************************/
     var DAILY_PATH = 'http://g.assets.daily.taobao.net';
-    var CDN_PATH = 'http://g.tbcdn.cn';
+    //var CDN_PATH = 'http://g.tbcdn.cn';
+    var CDN_PATH = DAILY_PATH;
     //grunt build 时使用build目录，本地打包调试使用
     var buildDir = 'build';
-    var buildBase, buildEnv = 'daily';
+  // Project configuration.
+    grunt.initConfig({
+        versionSrcBase: '.version',
+        versionBuildBase: buildDir ,//+ '/version',
+        jshint: {
+          all: [
+            'Gruntfile.js',
+            'tasks/**.js',
+          ],
+          options: {
+            jshintrc: '.jshintrc'
+          }
+        },
 
-    // load the contrib module
-    require('load-grunt-tasks')(grunt);
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: [ buildDir ]
+        },
 
-    grunt.registerTask('config', 'config the build', function() {
+        // Configuration to be run (and then tested).
+        incrediff: {
+            incre: {
+                options: {
+                    version: version,   //版本号数据 
 
-        grunt.initConfig({
-            pkg: pkg,
-            version: version,     /*配置增量更新的版本,一般来说，
-                                  创建完新分支之后修改下version.json到最新就可以了
-                                  目前是 ["1.3.6"]*/
-            jsSrcBase: 'src/js',
-            jsBuildBase: buildDir + '/js',
-            jsSrcExpand: '<%= jsSrcBase %>/libs_expand',
-            cssSrcBase: 'src/css',
-            cssBuildBase: buildDir + '/css',
-            //清理文件
-            clean: {
-                build: [buildDir + '/js', buildDir + '/css', buildDir + '/version']
-            },
-            versionSrcBase: '.version',
-            versionBuildBase: buildDir + '/version',
-            //编译sass
-            sass: {
-                dist: {
-                    expand: true,
-                    cwd: '<%= cssSrcBase %>',
-                    src: ['**/*.scss'],
-                    dest: '<%= cssSrcBase %>/<%= grunt.task.current.args[0] %>/',
-                    ext: '.css'
+                    chunkSize: 20,          //Diff生成配置
+                    isSingleDiff: false,
+                    dest: '<%= versionBuildBase %>',
+                    format: '%{NEWVERSION}/%{FILEPATH}_%{OLDVERSION}_%{NEWVERSION}', //支持FILEPATH,OLDVERSION,NEWVERSION三个替换
+                    //最后产生的可能是 g.tbcdn.cn/dd/h5/1.3.6/css/app_all.min.css_1.3.51
+                    //发布到线上需要把改成！！%{FILEPATH}_%{OLDVERSION}_%{NEWVERSION}！！，去掉最前面的 %{NEWVERSION}/
+
+                    newsrc: buildDir,   //new版本获取配置
+
+                    cdnUrl: CDN_PATH +'/dd/h5',   //old版本下载配置
+                },
+                files: {
+                    src: ['css/app_all.min.css', 'js/core_all.js', 'js/app/app_all.js']
                 }
             },
+      }
 
-            //合并
-            concat: {
-                /*base: {
-                 src: ['<%= jsSrcBase %>/libs_base/*.js', '<%= jsSrcBase %>/core.js'],
-                 dest: '<%= jsBuildBase %>/core_base_debug.js'
-                 },*/
-                all: {
-                    src: ['<%= jsSrcExpand %>/zepto.history.js', '<%= jsSrcBase %>/bridge.js', '<%= jsSrcBase %>/core.js', '<%= jsSrcBase %>/global/*.js', '<%= jsSrcBase %>/global/**/*.js', '<%= jsSrcExpand %>/*.js'],
-                    dest: '<%= jsBuildBase %>/core_all_debug.js'
-                },
-                app: {
-                    src: ['<%= jsSrcBase %>/app/*/*.js', '<%= jsSrcBase %>/app/app.js'],
-                    dest: '<%= jsBuildBase %>/app/app_all_debug.js'
-                }
-            },
-            //压缩
-            uglify: {
-                build: {
-                    files: {
-                        /*'<%= jsBuildBase %>/core_base.js': ['<%= concat.base.dest %>'],*/
-                        '<%= jsBuildBase %>/core_all.js': ['<%= concat.all.dest %>'],
-                        '<%= jsBuildBase %>/app/app_all.js': ['<%= concat.app.dest %>']
-                    }
-                }
-            },
-            /*
-             * https://github.com/daxingplay/grunt-css-combo
-             */
-            css_combo: {
-                options: {},
-                page: {
-                    files: {
-                        '<%= cssBuildBase %>/app_all.css': ['<%= cssSrcBase %>/app_all.css']
-                    }
-                }
-            },
-            /*
-             * https://github.com/gruntjs/grunt-contrib-cssmin
-             */
-            cssmin: {
-                combine: {
-                    files: {
-                        '<%= cssBuildBase %>/app_all.min.css': ['<%= cssBuildBase %>/app_all.css']
-                    }
-                }
-            },
-            /***********************************************************/
-            http: {
-                app_css: {
-                    options: {
-                        url: CDN_PATH + '/dd/h5/<%= version[1] %>/css/app_all.min.css',
-                    },
-                    files: {
-                        '<%= versionSrcBase %>/<%= version[1] %>/css/app_all.min.css': 'none'
-                    }
-                },
-                core_js: {
-                    options: {
-                        url: CDN_PATH + '/dd/h5/<%= version[1] %>/js/core_all.js',
-                    },
-                    files: {
-                        '<%= versionSrcBase %>/<%= version[1] %>/js/core_all.js': 'none'
-                    }
-                },
-                app_js: {
-                    options: {
-                        url: CDN_PATH + '/dd/h5/<%= version[1] %>/js/app/app_all.js',
-                    },
-                    files: {
-                        '<%= versionSrcBase %>/<%= version[1] %>/js/app/app_all.js': 'none'
-                    }
-                },
-            },
-            incrediff: {
-                incre: {
-                    options: {
-                        version: version,
-                        chunkSize: 20,
-                        dest: '<%= versionBuildBase %>',
-                        newsrc: buildDir,
-                        oldsrc: '<%= versionSrcBase %>',
-                        format: '%{FILEPATH}_%{OLDVERSION}_%{NEWVERSION}' //支持FILEPATH,OLDVERSION,NEWVERSION三个替换
-                        //最后产生的可能是 g.tbcdn.cn/dd/h5/1.3.6/.package/version/css/app_all.min.css_1.3.51
-                    },
-                    files: {
-                        src: ['css/app_all.min.css', 'js/core_all.js', 'js/app/app_all.js']
-/*                      'css/app_all.min.css' : ['css/app_all.min.css'],
-                      'js/core_all.js' : ['js/core_all.js'],
-                      'js/app/app_all.js' : ['js/app/app_all.js']*/
-                    }
-                },
-            }
-
-        });
-    })
-
-    grunt.registerTask('build_group', ['sass','clean', 'concat', 'uglify', 'css_combo', 'cssmin', 'http', 'incrediff']);
-    grunt.registerTask('default', ['init', 'config', 'build_group']);
-    grunt.registerTask('build', ['config', 'build_group']);
-
-    grunt.registerTask('init', 'init the build', function() {
-        buildDir = String(grunt.option('buildTo') || '.package').trim();
-        buildBase = String(grunt.option('path') || '').trim();
-
-        if (buildBase.indexOf(DAILY_PATH) != -1) {
-            buildEnv = 'daily';
-        }
-        if (buildBase.indexOf(CDN_PATH) != -1) {
-            buildEnv = 'publish';
-        }
-
-        if (!buildEnv) {
-            grunt.log.error('非法的base打包路径');
-            return false;
-        }
-
-        grunt.log.oklns('build to ' + buildDir + '[' + buildBase + ']');
     });
-}
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('test', ['clean', 'incrediff'/*, 'nodeunit'*/]);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/incrediff.js
@@ -0,0 +1,175 @@
+/*
+ * grunt-incrediff
+ * https://github.com/SenYu/grunt-incrediff
+ *
+ * Copyright (c) 2014 SenYu
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+var request = require('request'),
+    async = require('async'),
+    maxmin = require('maxmin'),
+    chalk = require('chalk'),
+    diff = require('./lib/newChunk');
+
+module.exports = function(grunt) {
+
+function responseHandler(dest, done, container, _name, url, cnt, all) {
+      return function (error, response, body) {
+            response = response || { statusCode: 0 };
+
+            if (error) {
+                return done(error);
+            } else if ( (response.statusCode < 200 || response.statusCode > 399)) {
+                grunt.log.warn( 'Download ' + chalk.grey(url) + chalk.red(' Failed ' + response.statusCode));
+                return done(response.statusCode);
+            }
+
+            container[ _name ] = body;
+            grunt.log.warn( 'Download ' + chalk.grey(url) + chalk.green(' success ') + (++cnt.cnt + '/' + all) );
+            done();
+      };
+}
+
+function strFormat( source, kvd ) {
+    if ( typeof source !== 'string' ) { return ''; }
+    for ( var key in kvd ) {
+        if ( kvd.hasOwnProperty( key ) ) {
+            source = source.replace( new RegExp('%{'+ key +'}','g') , kvd[ key ] );
+        }
+    }
+    return source;
+}
+
+    grunt.registerMultiTask('incrediff', 'auto-generate tool of incremental difference of static files', function() {
+        var options = this.options({
+            version: [],
+            chunkSize: 20,
+            isSingleDiff: true,
+            format: '%{FILEPATH}_%{OLDVERSION}' //支持FILEPATH,OLDVERSION,NEWVERSION三个替换
+        });
+
+        var i, j, len, lenJ;
+        var _versions;
+        var src;
+        var newContent;
+        var done;
+        if ( !options.dest || !options.newsrc || !options.cdnUrl ){
+            grunt.log.warn('config need [DEST],[NEWSRC],[CDNURL]');
+            return;
+        }
+
+        options.dest   = options.dest.replace(/\/$/,'') + '/';
+        options.newsrc = options.newsrc.replace(/\/$/,'') + '/';
+        options.cdnUrl = options.cdnUrl.replace(/\/$/,'') + '/';
+
+        if (  options.version.length  < 1 ) {
+            grunt.log.warn('Array[Version] must has 2 element AT LEAST');
+            return;
+        }
+
+        _versions = options.isSingleDiff ? [ options.version[ 0 ], options.version[ 1 ] || undefined ] : options.version;
+            if ( typeof _versions[ _versions.length -1 ] === 'undefined' ) {
+                _versions.pop();
+            }
+            _versions = _versions.concat( [''] );
+        //需要生成差异的版本数组,最后有一个 ''元素
+        src = this.files[0].orig.src;
+        src = src.map(function (data) {
+            return data.replace(/^\//,'');
+        });
+        //原文件path数组
+        newContent = [];
+        //源文件path的content数组
+        done = this.async();
+        //async 同步
+
+        for ( j = 0, len = src.length ; j < len ; j++ ) {
+            //read 最新版本文件存入newContent
+            var readPath = options.newsrc + src[ j ];
+            newContent[ j ] = grunt.file.read(readPath);
+        }
+
+        //生成request请求的url队列
+        var reqQueue = [];
+        for ( i = 1, len = _versions.length; i < len ; i ++) {
+            var curVersion = _versions[ i ];
+            if ( curVersion !== '' ) {
+                for ( j = 0, lenJ = src.length; j < lenJ ; j++ ) {
+                    var oldUrl = options.cdnUrl + curVersion + '/' + src[ j ];
+                    reqQueue.push( {url:oldUrl ,name: src[j] + '?' + curVersion} );
+                }
+            }
+        }
+
+        //请求回来的内容给reqContent,全部请求完成后执行 resolve
+        var reqContent = {};
+        var _count = {cnt:0};
+        async.each(reqQueue, 
+            function (file, next) {
+                 var r, callback;
+                 var url = file.url,
+                     name = file.name;
+
+                 callback = responseHandler(null, next, reqContent, name, url, _count, reqQueue.length);
+                 //写入文件的回调放在 responseHandler 里面了
+                 r = request({url: url}, callback);
+
+            }, 
+            //操作结束 (均完成或有一失败),执行下面的,开始生成
+            function (err) {
+                if (err) {    
+                    grunt.log.writeln(chalk.red('ERROR 404: ')+chalk.yellow('Please Validate ths Configuration of CDN-URL or File Path'));
+                    grunt.fail.fatal(err);
+                }
+                else {
+                    grunt.log.warn('NEXT');
+                }
+                //TODO, 开始产生差分数据
+                for ( i = 1, len = _versions.length; i < len ; i ++) {
+                    var curVersion = _versions[ i ];
+                    
+                    for ( j = 0, lenJ = src.length ; j < lenJ ; j ++) {
+                        if ( curVersion === '' ) {
+                            generateFullData( newContent[ j ], src[ j ] );
+                        }
+                        else {
+                            //Http下载
+                            var oldContent = reqContent[ src[ j ] + '?' + curVersion ];
+                            generateDiffData( newContent[ j ], oldContent, curVersion, src[ j ] );
+                        }
+                    }
+                }
+
+                done(err);
+            });
+
+        function generateDiffData ( newdata, olddata, oldver, path) {
+            var writePath = options.dest + strFormat( options.format, {
+                FILEPATH: path,
+                OLDVERSION: oldver,
+                NEWVERSION: _versions[0]
+            } );
+
+            var content = diff(olddata, newdata, options.chunkSize);
+            var writeContent = JSON.stringify( content ) + '/*"""*/';
+            grunt.file.write(writePath, writeContent, 'utf8');
+            grunt.log.warn('DiffData '+ chalk.cyan(writePath) + ' created: ' + maxmin(newdata, writeContent, false)  );
+        }
+
+        function generateFullData ( content, path ) {
+            var writePath = options.dest + strFormat( options.format, {
+                FILEPATH: path,
+                OLDVERSION: '',
+                NEWVERSION: _versions[0]
+            } );
+
+            var writeContent = JSON.stringify( content ) + '/*"""*/';
+            grunt.file.write(writePath, writeContent, 'utf8');
+            grunt.log.warn('FullData '+ chalk.cyan(writePath) + ' created ' );
+        }
+
+    });
+
+};
